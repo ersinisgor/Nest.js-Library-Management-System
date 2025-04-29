f78e25ba10921cf0da854c693daaac05655c13f5
Ersin Isgor <ersinisgor@hotmail.com>
2025-04-29 15:52:52 +0300

    feat: add Book entity to Author module imports for enhanced relations

--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -3,9 +3,10 @@ import { AuthorService } from './author.service';
 import { AuthorController } from './author.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Author } from './entity/author.entity';
+import { Book } from 'src/book/entity/book.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Author])],
+  imports: [TypeOrmModule.forFeature([Author, Book])],
   controllers: [AuthorController],
   providers: [AuthorService],
 })
