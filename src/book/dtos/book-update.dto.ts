d0e2e075868cbe9e1a01299a185d15099d12321f
Ersin Isgor <ersinisgor@hotmail.com>
2025-04-30 10:45:38 +0300

    feat: add @nestjs/mapped-types dependency and refactor UpdateBookDTO to extend PartialType

--- src/book/dtos/book-update.dto.ts
+++ src/book/dtos/book-update.dto.ts
@@ -1,26 +1,4 @@
-import { IsBoolean, IsInt, IsOptional, IsString, Min } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateBookDTO } from './book-create.dto';
 
-export class UpdateBookDTO {
-  @IsString()
-  @IsOptional()
-  title?: string;
-
-  @IsString()
-  @IsOptional()
-  isbn?: string;
-
-  @IsInt()
-  @Min(1, {
-    message: 'pageCount must be greater than at least $constraint1 page.',
-  })
-  @IsOptional()
-  pageCount?: number;
-
-  @IsBoolean()
-  @IsOptional()
-  isAvailable?: boolean;
-
-  @IsInt()
-  @IsOptional()
-  authorId?: number;
-}
+export class UpdateBookDTO extends PartialType(CreateBookDTO) {}
