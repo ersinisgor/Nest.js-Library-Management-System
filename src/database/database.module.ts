fcdeaaf81b85f17727bfd6281732c8afaa5d9d6d
Ersin Isgor <ersinisgor@hotmail.com>
2025-05-01 09:46:48 +0300

    feat: include User entity in DatabaseModule and update UserModule imports

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Author } from '../author/entity/author.entity';
 import { Book } from '../book/entity/book.entity';
+import { User } from 'src/user/entity/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book, Author])],
+  imports: [TypeOrmModule.forFeature([Book, Author, User])],
   exports: [TypeOrmModule],
 })
 export class DatabaseModule {}
