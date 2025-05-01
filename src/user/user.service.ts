06ab36d8133d795e41d21c920e14383741997dfc
Ersin Isgor <ersinisgor@hotmail.com>
2025-05-01 16:43:50 +0300

    feat: implement UserService with createUser method and conflict handling

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { QueryFailedError, Repository } from 'typeorm';
+import { CreateUserDTO } from './dtos/user-create.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDTO: CreateUserDTO): Promise<User> {
+    try {
+      const user = this.userRepository.create(createUserDTO);
+      return await this.userRepository.save(user);
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        const driverError = error.driverError as { code?: string };
+        if (driverError.code === '23505') {
+          throw new ConflictException('A User with this email already exists');
+        }
+      }
+      throw error;
+    }
+  }
+}
