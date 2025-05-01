24fb5d293e49114c5865d839af2a3b5bf6db2ca2
Ersin Isgor <ersinisgor@hotmail.com>
2025-05-01 16:44:09 +0300

    feat: implement createUser method in UserController with DTO handling

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/user-create.dto';
+import { UserService } from './user.service';
+import { User } from './entity/user.entity';
 
 @Controller('users')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() createUserDTO: CreateUserDTO): Promise<User> {
+    return await this.userService.createUser(createUserDTO);
+  }
+}
