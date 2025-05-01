f6de6a19df2a0e0d4b267b304cce0d5115799e12
Ersin Isgor <ersinisgor@hotmail.com>
2025-05-01 16:44:36 +0300

    fix: correct import paths for User and DatabaseModule in respective modules

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Author } from '../author/entity/author.entity';
 import { Book } from '../book/entity/book.entity';
-import { User } from 'src/user/entity/user.entity';
+import { User } from '../user/entity/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book, Author, User])],
