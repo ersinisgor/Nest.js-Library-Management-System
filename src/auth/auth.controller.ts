c27ac39597c8a0b4e3f8c456cf57f5431e74d5c2
Ersin Isgor <ersinisgor@hotmail.com>
2025-05-04 04:48:24 +0300

    feat: enhance login endpoint with error handling for better user feedback

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  InternalServerErrorException,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDTO } from './dtos/register.dto';
 import { UserResponseDTO } from '../user/dtos/user-response.dto';
+import { LoginDTO } from './dtos/login.dto';
+import { LoginResponseDto } from './dtos/login-response.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +21,18 @@ export class AuthController {
   @HttpCode(201)
   async register(@Body() registerDTO: RegisterDTO): Promise<UserResponseDTO> {
     return await this.authService.register(registerDTO);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() loginDTO: LoginDTO): Promise<LoginResponseDto> {
+    try {
+      return await this.authService.login(loginDTO);
+    } catch (error) {
+      if (error instanceof BadRequestException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Failed to login');
+    }
   }
 }
