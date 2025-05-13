9cd6c1c20f09580aaaad00294f54c3e5af9d6c16
Ersin Isgor <ersinisgor@hotmail.com>
2025-05-13 10:33:28 +0300

    chore: add commented imports for RolesGuard setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// import { RolesGuard } from './guards/roles.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // const reflector = app.get<Reflector>(Reflector);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +14,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // app.useGlobalGuards(new RolesGuard(reflector));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
